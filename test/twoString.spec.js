550b002d9a9b024f84413650b0cfa45a156cef43
Martin Lehmann <lehmar11@student.westerdals.no>
2016-02-08 22:13:39 +0100

    Support Node v0.12.x

--- test/twoString.spec.js
+++ test/twoString.spec.js
@@ -2,8 +2,8 @@ const expect = require('chai').expect;
 
 const twoString = require('../src/index.js');
 
-describe('twoString', () => {
-  it('should return a two string', () => {
+describe('twoString', function testTwoString() {
+  it('should return a two string', function checkReturnsTwoString() {
     const result = twoString();
     expect(result).to.equal('2');
   });
